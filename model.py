082c8d7dcd425cba24323d4bf8bb53ad3c8c014b
saisumanthkorthiwada <saisumanthkorthiwada@gmail.com>
2021-05-21 21:10:02 +0530

    Model changed in imports

--- model.py
+++ model.py
@@ -1,12 +1,13 @@
-import os
-from keras.preprocessing import image
-import matplotlib.pyplot as plt 
-import numpy as np
-from keras.utils.np_utils import to_categorical
-import random,shutil
 from keras.models import Sequential
 from keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
 from keras.models import load_model
+import os
+import numpy as np
+from keras.preprocessing import image
+import matplotlib.pyplot as plt 
+from keras.utils.np_utils import to_categorical
+import random,shutil
+
 
 
 def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
